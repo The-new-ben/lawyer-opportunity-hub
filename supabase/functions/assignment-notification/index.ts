05b38f44932c5b9d9172642bbda0170da03c5716
The-new-ben <124096480+The-new-ben@users.noreply.github.com>
2025-08-03 20:48:21 +0300

    Unify Supabase service role env var

--- supabase/functions/assignment-notification/index.ts
+++ supabase/functions/assignment-notification/index.ts
@@ -21,7 +21,7 @@ serve(async (req) => {
 
     const supabaseAdmin = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE') ?? '',
       { auth: { persistSession: false } }
     );
 
